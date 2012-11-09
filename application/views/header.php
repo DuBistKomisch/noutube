83855126fa316c7321b702df8acd37becf972715
Jake Barnes <jakerodneybarnes@gmail.com>
2012-11-09 01:02:07 +0000

    Added favicon to header

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title><?php echo $applicationName . (isset($pageName) ? ' &ndash; ' . $pageName : ''); ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo site_url('css/template.css'); ?>">
+    <link rel="icon" type="image/png" href="<?php echo site_url('favicon.png')?>">
   </head>
   <body>
     <h1><?php echo anchor(site_url(), $applicationName); ?></h1>
