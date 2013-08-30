51bfc087be5b733438be46a4a8a2a64c85bacfcf
Jake Barnes <jakerodneybarnes@gmail.com>
2013-08-30 14:22:46 +1000

    Fixed checkboxes: added defaults and ids so labels are clickable

--- application/views/videos/video.php
+++ application/views/videos/video.php
@@ -5,8 +5,8 @@
     <p class="update" title="<?php echo $published_date; ?>">Published <?php echo $published_ago; ?> ago</p>
   </div>
 <?php if ($form === 'new'): ?>
-  <p><?php echo form_checkbox('new' . $video) . form_label('Watch later', 'new' . $video); ?></p>
+  <p><?php echo form_checkbox('new' . $video, '', true, 'id="new' . $video . '"') . form_label('Watch later', 'new' . $video); ?></p>
 <?php elseif ($form === 'later'): ?>
-  <p><?php echo form_checkbox('later' . $video) . form_label('Watched', 'later' . $video); ?></p>
+  <p><?php echo form_checkbox('later' . $video, '', false, 'id="later' . $video . '"') . form_label('Watched', 'later' . $video); ?></p>
 <?php endif; ?>
 </div>
